719cc78f2f00c840f40c142a452794f1f0b82eb6
Ivan Markovski <sagitariusim@gmail.com>
2024-06-20 00:33:06 +0200

    updates

--- src/components/AddDailySchedule.jsx
+++ src/components/AddDailySchedule.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import {useNavigate, useOutletContext} from "react-router-dom";
 
@@ -19,5 +20,61 @@ const AddDailySchedule = () => {
         employees: [],
         shifts: [],
     })
+
+    const [position_name, setPositionName] = useState([])
+    const [employee, setEmployee] = useState([])
+    const [shift, setShift] = useState([])
+
+    useEffect(() => {
+        if (jwtToken === "") {
+            nav("/login")
+        }
+
+        const header = new Headers()
+        header.append('Content-Type', 'application/json')
+        header.append("Authorization", "Bearer " + jwtToken);
+
+        const requestOptions = {
+            method: "GET",
+            headers: header,
+        }
+
+        fetch(`/positions`, requestOptions)
+        .then((response) => {
+            if (response.status !== 200) {
+                setError("Invalid response code: " + response.status)
+            }
+            return response.json();
+        })
+        .then((data => {
+            setPositionName(data)
+        }))
+        .catch(err => console.log(err));
+
+        fetch(`/all-users`, requestOptions)
+        .then((response) => {
+            if (response.status !== 200) {
+                setError("Invalid response code: " + response.status)
+            }
+            return response.json();
+        })
+        .then((data) => {
+            setEmployee(data)
+        })
+        .catch(err => console.log(err));
+
+        fetch(`/all-shifts`, requestOptions)
+        .then((response) => {
+            if (response.status !== 200) {
+                setError("Invalid response code: " + response.status)
+            }
+            return response.json();
+        })
+        .then((data) => {
+            setShift(data)
+        })
+        .catch(err => console.log(err));
+
+    }, [jwtToken, nav])
 }
 export default AddDailySchedule
